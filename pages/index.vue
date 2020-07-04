24737fd1d0552b59b507a5c5e749fd5c733625da
mcbianconi <murillo.bianconi@gmail.com>
2020-07-04 09:34:34 -0300

    first attempts with aws

--- pages/index.vue
+++ pages/index.vue
@@ -27,6 +27,7 @@ import RegistrationForm from '~/components/registration-form'
 import EventInfo from '~/components/event-info'
 import EventAbout from '~/components/event-about'
 import PageFooter from '~/components/page-footer'
+import AWS from 'aws-sdk';
 
 export default {
   transition: 'slide-right',
@@ -36,18 +37,42 @@ export default {
     EventInfo,
     EventAbout,
     PageFooter
+  },
+
+  async fetch () {
+
+    //
+    //  1.  Create the params of the object we want in S3
+    //
+    let params = {
+      Bucket: "webinars.0x4447.com.db.events",
+      Key: "latest_description.json"
+    }
+
+    //
+    //  2.  Generate the AWS object with the Unuthorized Cognito credentials.
+    //
+    AWS.config.update({
+      region: "us-east-1",
+      credentials: new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: "us-east-1:619639e5-f742-4d8a-a190-850c65bb344e"
+      })
+    })
+
+    //
+    //  3.  Initialize S3 Object
+    //
+    var s3 = new AWS.S3();
+
+    //
+    //  4.  Get the object we want
+    //
+    s3.getObject(params, function(err, data) {
+      debugger
+      if (err) console.log(err, err.stack); // an error occurred
+      else     console.log(data);           // successful response
+    })
   }
 
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-</style>
