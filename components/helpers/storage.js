26483d2ca61ad48a7cdfee4f5eaadf55b6ae5e94
David Gatti <github@gatti.io>
2020-07-05 10:16:23 +0200

    Simplification and AWS class change

--- components/helpers/storage.js
+++ components/helpers/storage.js
@@ -1,32 +1,24 @@
 import AWS from 'aws-sdk'
 
-let TEST = true
-let TEST_REGION = "us-east-2"
-let TEST_BUCKET = "cognito-teste"
-let TEST_POOL_ID = "us-east-2:4fb6f5d4-3388-4da5-b4d2-2bedca19f11b"
-
-
-let REGION = TEST ? TEST_REGION : "us-east-1"
-let BUCKET = TEST ? TEST_BUCKET : "webinars.0x4447.com.db.events"
-let POOL_ID = TEST ? TEST_POOL_ID : "us-east-1:619639e5-f742-4d8a-a190-850c65bb344e"
-
+let BUCKET = "webinars.0x4447.com.db.events"
 let EVENT_KEY = "latest_description.json"
 let EVENT_TIME_KEY = "latest_time.json"
-let API_VERSION = '2006-03-01'
 
 //
 //  1.  Generate the AWS object with the Unuthorized Cognito credentials.
 //
-AWS.config.region = REGION
-AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: POOL_ID,
-})
+AWS.config.update({
+  region: "us-east-1",
+  credentials: new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: "us-east-1:619639e5-f742-4d8a-a190-850c65bb344e"
+  })
+});
 
 //
 //  2.  Initialize S3 Object.
 //
 let s3 = new AWS.S3({
-  apiVersion: API_VERSION
+  apiVersion: '2006-03-01'
 })
 
 //
