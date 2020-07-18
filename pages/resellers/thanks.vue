18d1ab071c46b56476c0e3101efb30fbb25d62c8
mcbianconi <murillo.bianconi@gmail.com>
2020-07-18 11:33:00 -0300

    Fine tunes

---
+++ pages/resellers/thanks.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-container>
+    <h1>Thank You!</h1>
+    <br />
+    <b-button variant="primary" @click="go_back">Go Back</b-button>
+  </b-container>
+</template>
+
+<script>
+export default {
+  transition: 'slide-left',
+  methods: {
+    go_back () {
+      this.$router.push({ path: '/' })
+    }
+  }
+}
+</script>
+
