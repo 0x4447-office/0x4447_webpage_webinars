6a1c80a0cc0c12612e0f1c2b4e9b27c1f8532dd6
David Gatti <github@gatti.io>
2020-07-27 11:19:10 +0200

    Added real data

--- components/helpers/reseller.js
+++ components/helpers/reseller.js
@@ -1,5 +1,5 @@
 export const AVAILABLE_PRODUCTS = [
-    { name: "OpenVPN", id: "23123123" },
-    { name: "Example 2", id: "2" },
-    { name: "Example 3", id: "3" }
+    { name: "OpenVPN", id: "B0839R5C7Z" },
+    { name: "Rsyslog", id: "B07YN9CCV4" },
+    { name: "Samba", id: "B084HF14KL" }
 ];
